1a05f522a529505c4078bce9ffe48c3cbd3e1393
Zachary Wasserman <Zdwasserman@gmail.com>
2016-11-29 16:48:14 -0500

    able to add employees to db

--- src/main/java/com/hrapplication/Controller/EmployeeController.java
+++ src/main/java/com/hrapplication/Controller/EmployeeController.java
@@ -1,9 +1,44 @@
 package com.hrapplication.Controller;
 
+import com.hrapplication.Model.Employee;
+import com.hrapplication.Repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 /**
  * Created by zacharywasserman on 11/28/16.
  */
+
+@CrossOrigin("http://localhost:9000")
+@RestController
 public class EmployeeController
 {
+
+    @Autowired
+    EmployeeRepository employeeRepo;
+
+    //@Autowired
+    //Employee employee;
+
+    public EmployeeController(EmployeeRepository employeeRepo)
+    {
+        this.employeeRepo = employeeRepo;
+        //this.employee = employee;
+    }
+
+    @RequestMapping(value = "/createEmployee", method = RequestMethod.POST)
+    public Employee createEmployee(@RequestBody Employee employee)
+    {
+        employeeRepo.save(employee);
+        return employee;
+    }
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<Employee> getEmployee()
+    {
+        return employeeRepo.findAll();
+    }
 
 }
